2f307e0b9a2a9f2e4398cbd0cbe1821bdfa4fe12
hosaka0x0 <hosaka0x0@gmail.com>
2026-02-13 17:00:26 +0900

    feat:top page bento setup

---
+++ src/components/Cards/IconCard.jsx
@@ -0,0 +1,12 @@
+function IconCard(){
+    return(
+    
+    <div className="bento-card bento-card-2x2">    
+        <img
+            src="/portfolio/images/portfolio.jpg"
+            alt="Profile"
+            />
+    </div>
+            )
+    }
+export default IconCard
